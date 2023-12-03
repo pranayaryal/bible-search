ff458b8e6900c0715e9ca733cb7fde540ed16300
pranayaryal <drpranayaryal@gmail.com>
2023-12-03 17:15:03 -0500

    mobile responseve

--- client/components/Dropdown.js
+++ client/components/Dropdown.js
@@ -13,7 +13,7 @@ const Dropdown = ({ setDropDown, setDropVal, dropVal, dropDown }) => {
         <button
           onClick={() => setDropDown(!dropDown)}
           type="button" 
-          className="w-full inline-flex justify-start rounded-md bg-white px-3 py-2 items-center text-sm font-semibold text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          className="w-full inline-flex justify-start rounded-md bg-white px-3 py-2 items-center text-xs lg:text-sm font-semibold text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           {`Verses to show ${dropVal}`}
           <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path filrule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
